d66b2cb5615b34651979cbfad2c857bb6ab3e44c
bnates <brian.nations@codefellows.com>
2018-02-26 11:56:37 -0800

    added demo

---
+++ 11-express/demo/lib/storage.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), { suffix: 'Prom' });
+const createError = require('http-errors');
+
+module.exports = exports = {};
+
+exports.createItem = function(schemaName, item) {
+  if (!schemaName) return Promise.reject(createError(400, 'expected schema name'));
+  if (!item) return Promise.reject(createError(400, 'expected item'));
+
+  let json = JSON.stringify(item);
+  return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json)
+  .then( () => item)
+  .catch( err => Promise.reject(err));
+}
+
+exports.fetchItem = function(schemaName, id) {
+  if (!schemaName) return Promise.reject(createError(400, 'expected schema name'));
+  if (!id) return Promise.reject(createError(400, 'expected id'));
+
+  return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+  .then( data => {
+    try {
+      let item = JSON.parse(data.toString());
+      return item;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  })
+  .catch( err => Promise.reject(err));
+}
---
+++ 11-express/demo/server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('note:server');
+const Note = require('./model/note.js');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/test', function(req, res) {
+  debug('GET: /test');
+  res.json({ msg: 'hello from /test' });
+});
+
+app.post('/api/note', jsonParser, function(req, res, next) {
+  debug('POST: /api/note');
+  Note.createNote(req.body)
+  .then( note => res.json(note))
+  .catch( err => next(err));
+});
+
+app.get('/api/note/:noteId', function(req, res, next) {
+  debug('GET: /api/note/:noteId');
+
+  Note.fetchNote(req.params.noteId)
+  .then( note => res.json(note))
+  .catch( err => next(err));
+});
+
+app.use(function(err, req, res, next) {
+  debug('error middleware');
+  console.error(err.message);
+
+  if (err.status) {
+    res.status(err.status).send(err.name);
+    return
+  }
+
+  err = createError(500, err.message);
+  res.status(err.status).send(err.name);
+});
+
+app.listen(PORT, () => {
+  debug(`server up: ${PORT}`);
+});

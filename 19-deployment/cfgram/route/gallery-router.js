4bbbe126dd7e9ea6e98d012b7b4cb57d2029d3c8
bnates <brian.nations@codefellows.com>
2018-03-08 11:52:44 -0800

    added demo

---
+++ 19-deployment/cfgram/route/gallery-router.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
+const debug = require('debug')('cfgram:gallery-router');
+
+const Gallery = require('../model/gallery.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+
+const galleryRouter = module.exports = Router();
+
+galleryRouter.post('/api/gallery', bearerAuth, jsonParser, function(req, res, next) {
+  debug('POST: /api/gallery');
+
+  req.body.userID = req.user._id;
+  new Gallery(req.body).save()
+  .then( gallery => res.json(gallery))
+  .catch(next);
+});
+
+galleryRouter.get('/api/gallery/:galleryId', bearerAuth, function(req, res, next) {
+  debug('GET: /api/gallery/:galleryId');
+  
+  Gallery.findById(req.params.galleryId)
+  .then( gallery => res.json(gallery))
+  .catch(next);
+});

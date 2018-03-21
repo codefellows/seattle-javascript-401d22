a0a53b50c4f5a0e9371abf4dd8cc5ed4757d843b
bnates <brian.nations@codefellows.com>
2018-03-21 12:25:06 -0700

    added demo

---
+++ 28-routing/budget-tracker/src/component/about-container/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+class AboutContainer extends React.Component {
+  render() {
+    return (
+      <div className='about-container'>
+        <p>some cool things about me</p>
+      </div>
+    )
+  }
+}
+
+export default AboutContainer;

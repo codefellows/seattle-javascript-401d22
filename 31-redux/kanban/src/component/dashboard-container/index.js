76464b0581fb2845f75dfcff3e89e0d32a4b8216
bnates <brian.nations@codefellows.com>
2018-03-26 12:20:42 -0700

    added demo

---
+++ 31-redux/kanban/src/component/dashboard-container/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {
+  categoryCreate,
+  categoryUpdate,
+  categoryDelete
+} from '../../action/category-actions.js';
+
+import CategoryForm from '../category-form';
+
+class DashboardContainer extends React.Component {
+  componentDidMount() {
+    this.props.categoryCreate({ title: 'test category' });
+    this.props.categoryCreate({ title: 'another test' });
+  }
+
+  render() {
+    return (
+      <main className='dashboard-container'>
+        <h2>Dashboard</h2>
+
+        <CategoryForm
+          buttonText='create a category'
+          onComplete={this.props.categoryCreate} />
+
+        {this.props.categories.map(item =>
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+          </div>
+        )}
+      </main>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    categories: state
+  }
+}
+
+const mapDispatchToProps = (dispatch, getState) => {
+  return {
+    categoryCreate: (category) => dispatch(categoryCreate(category)),
+    categoryUpdate: (category) => dispatch(categoryUpdate(category)),
+    categoryDelete: (category) => dispatch(categoryDelete(category))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
---
+++ 31-redux/kanban/src/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/app';
+
+ReactDom.render(<App />, document.getElementById('root'));
